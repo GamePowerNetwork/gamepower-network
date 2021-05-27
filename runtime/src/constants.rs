58050566637d2594fd216d2462ed3b1bd7b698a9
Mike Huntington <mellomike3000@gmail.com>
2021-05-27 18:58:32 -0400

    working on warp sync

--- runtime/src/constants.rs
+++ runtime/src/constants.rs
@@ -1,5 +1,7 @@
 pub mod currency {
-  use gamepower_primitives::{ Balance };
+  //use gamepower_primitives::{ Balance };
+
+  pub type Balance = u64;
 
   pub const DOLLARS: Balance = 1_000_000_000;
   pub const CENTS: Balance = DOLLARS / 10;
