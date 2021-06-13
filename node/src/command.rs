3146874d96f8d78c2950bcd7f6a76c7793223a34
Mike Huntington <mellomike3000@gmail.com>
2021-06-13 16:05:24 -0400

    testing warp sync

--- node/src/command.rs
+++ node/src/command.rs
@@ -129,8 +129,8 @@ pub fn run() -> sc_cli::Result<()> {
 			let runner = cli.create_runner(&cli.run)?;
 			runner.run_node_until_exit(|config| async move {
 				match config.role {
-					Role::Light => service::new_light(config),
-					_ => service::new_full(config),
+					Role::Light => service::new_light(config).map(|(task_manager, _)| task_manager),
+					_ => service::new_full(config).map(|(task_manager, _)| task_manager),
 				}.map_err(sc_cli::Error::Service)
 			})
 		}
